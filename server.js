35ae4a50c8f4d4421426d668e47900e9c351c62c
richknicks <rlynch@gmail.com>
2020-03-12 17:02:47 -0400

    set up validation

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ server.use('/api/posts',postRouter);
 server.use('/api/users',userRouter)
 server.use(express.json());
 
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
--- users/userRouter.js
+++ users/userRouter.js
@@ -3,19 +3,20 @@ const User = require('./userDb');
 // const shortid = require('shortid');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+
+router.post('/',validateUserId, (req, res) => {
   // do your magic!
-  User.insert(req.body)
+  const {id}=req.params
+  const body=req.body
+  User.insert(id, body)
   .then(post=>{
-    if(post){
-      res.status(201).json(post);
-    }else{
-      res.status(400).json({errorMessage: "Please provide title and contents for the post."})
-    }
+    post?
+      res.status(201).json(post):
+      res.status(400).json({errorMessage: "Please provide title and contents for the post."})    
   })
   .catch(error=>{
     console.log(error);
-    res.status(500).json({message: "There was an error while saving the post to the database"})
+    res.status(500).json({message: "There was an error while saving the user to the database"})
   })
 });
 
@@ -76,27 +77,66 @@ router.delete('/:id', (req, res) => {
   User.remove(id)
   .then(post=>{
     
-   post ? res.status(200).json(post) : res.status(404).json({error: "The post with the specified ID does not exist"})
+   post ? res.status(200).json(post) : res.status(404).json({error: "The user with the specified ID does not exist"})
  
 })
 .catch(error=>{
   console.log(error)
-  res.status(500).json({error: "The post information could not be retrieved"})
+  res.status(500).json({error: "The users information could not be retrieved"})
 });
 })
 router.put('/:id', (req, res) => {
   // do your magic!
+  const {id}=req.params
+  const changes=req.body
+  User.update(id, changes)
+  .then(post=>{
+    if(changes.name || changes.id){
+      res.status(200).json(post)
+    }else if(!changes.name || !changes.id) {
+      res.status(400).json({ errorMessage: "Please provide id and name for the user." })
+    }else{
+      res.status(404).json({ message: "The user with the specified ID does not exist." })
+    }
+  })
+  .catch(error=>{
+    console.log(error)
+    res.status(500).json({error: "This information cannot be accessed"})
+  })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const {id}=req.params
+  User.getById(id)
+  .then(post=>{
+    id?
+    res.status(200).json(post) :
+    res.status(400).json({error: 'Not validated'})
+  })
+  .catch(error=>{
+    console.log(error);
+    res.status(500).json({error: "This is a 500 error"})
+  })
+  next();
 
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  
+  User.get(req.body)
+  .then(post =>{
+    !body?res.status(400).json({message: "missing user data"}):
+    !post.name? res.status(400).json({ message: "missing required name field" });
+  })
+  .catch(error=>{
+    console.log(error);
+    res.status(500).json({error: "This is a 500 validation error"})
+  })
+  next();
 }
 
 function validatePost(req, res, next) {
